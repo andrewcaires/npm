462781b82ef30238d600bd2815d224cb1924fe7d
Andrew Caires <andrewcaires@live.com>
2022-06-22 11:21:04 -0300

    set "is" return types

--- utils.js/src/type.ts
+++ utils.js/src/type.ts
@@ -2,27 +2,27 @@ import { TypeObjectString } from "./typedef";
 
 const types: TypeObjectString = {};
 
-export const isArray = (test: any) => type(test) == "array";
+export const isArray = (test: any): test is Array<any> => type(test) == "array";
 
-export const isBoolean = (test: any) => type(test) == "boolean";
+export const isBoolean = (test: any): test is boolean => type(test) == "boolean";
 
-export const isDef = (test: any) => type(test) != "null";
+export const isDef = (test: any): test is any => type(test) != "null";
 
-export const isFunction = (test: any) => type(test) == "function";
+export const isFunction = (test: any): test is Function => type(test) == "function";
 
-export const isNumber = (test: any) => type(test) == "number";
+export const isNumber = (test: any): test is number => type(test) == "number";
 
-export const isObject = (test: any) => type(test) == "object";
+export const isObject = (test: any): test is object => type(test) == "object";
 
-export const isString = (test: any) => type(test) == "string";
+export const isString = (test: any): test is string => type(test) == "string";
 
-export const isFloat = (test: any) => isNumber(test) && !!(test % 1);
+export const isFloat = (test: any): test is number => isNumber(test) && !!(test % 1);
 
-export const isInteger = (test: any) => isNumber(test) && !(test % 1);
+export const isInteger = (test: any): test is number => isNumber(test) && !(test % 1);
 
-export const isNull = (test: any) => test === null;
+export const isNull = (test: any): test is null => test === null;
 
-export const isUndefined = (test: any) => test === undefined;
+export const isUndefined = (test: any): test is undefined => test === undefined;
 
 export const type = (test: any): string => test == null ? "null" : types[types.toString.call(test)] || "object";
 
